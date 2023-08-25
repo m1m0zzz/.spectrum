e5208b25bd60526904ed26f28989b7d13d428750
m1m0zzz <contact.m1m0zzz@gmail.com>
2023-08-25 14:46:24 +0900

    fix: sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,21 @@ const CACHE_NAME = "app-caches-v2";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./style.css",
+  "./styles/style.css",
+  "./styles/modern-css-reset/reset.min.css",
   "./main.js",
   "./assets/logo_192_trim.png",
+  "./assets/thumbnail.jpg",
+  "./assets/x12y12pxMaruMinya_2023-07-14/x12y12pxMaruMinya.ttf",
+  "./assets/icons/android-chrome-192x192.png",
+  "./assets/icons/android-chrome-512x512.png",
+  "./assets/icons/apple-touch-icon.png",
+  "./assets/icons/browserconfig.xml",
+  "./assets/icons/favicon-16x16.png",
+  "./assets/icons/favicon-32x32.png",
+  "./assets/icons/favicon.ico",
+  "./assets/icons/mstile-150x150.png",
+  "./assets/icons/safari-pinned-tab.svg",
 ];
 
 self.addEventListener("install", (event) => {
